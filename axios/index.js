0fe1959f2d407f50e38a32b43b92034bbf757891
Kranti <darkphoton20@gmail.com>
2023-02-02 17:29:58 +0800

    testing api

--- axios/index.js
+++ axios/index.js
@@ -1,3 +1,3 @@
 import createServer from 'next/dist/server/next';
 
-createServer('');
+export const api = createServer('');
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css';
-import api from '../axios';
+import { api } from '../axios';
 
 export default function Home() {
   const [data, setData] = useState({});
